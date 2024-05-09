e7c952e6f534a8190b7816f61b7395756588f7dc
Alex Tayron <alextayron@gmail.com>
2024-05-09 17:15:28 -0300

    Iniciando projeto com spring boot

---
+++ Finance-Saloon/src/main/java/com/finacesaloon/finance_saloon/FinanceSaloonApplication.java
@@ -0,0 +1,13 @@
+package com.finacesaloon.finance_saloon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceSaloonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceSaloonApplication.class, args);
+	}
+
+}
---
+++ Finance-Saloon/src/test/java/com/finacesaloon/finance_saloon/FinanceSaloonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finacesaloon.finance_saloon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceSaloonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
